bde91ebb6e376c3691d5a142fee9c53dd323da71
= <fadz_2022@hotmail.com>
2023-10-20 22:03:10 +0100

    added objects

--- js_objects.js
+++ js_objects.js
@@ -1,16 +1,32 @@
 
-    let player1 = {
-    firstName : "Fardowsa",
-    lastName : "Nur",
-    age: 17
+//     let player1 = {
+//     firstName : "Fardowsa",
+//     lastName : "Nur",
+//     age: 17
+// }
+
+// let player2 = {
+//     firstName: "Ikram",
+//     lastName: "Nur",
+//     age:10
+// }
+ 
+// console.log(player1.firstName)
+// console.log(player2.firstName)
+
+
+// //// // // // 
+
+let player1 = {
+    firstName : "Christiano",
+    lastName : "Ronaldo",
+    age : 38,
+    hasKids : true,
+
+    shoot : function () {
+        console.log( this.lastName + " shot the ball")
+    }
 }
 
-let player2 = {
-    firstName: "Ikram",
-    lastName: "Nur",
-    age:10
-}
- 
-console.log(player1.firstName)
-console.log(player2.firstName)
+console.log(player1.shoot())
 
