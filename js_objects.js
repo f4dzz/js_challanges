9b6735b837467b00e31553bd670e7794836f21f2
= <fadz_2022@hotmail.com>
2023-10-20 21:47:51 +0100

    i added arrow funtion

---
+++ js_objects.js
@@ -0,0 +1,16 @@
+
+    let player1 = {
+    firstName : "Fardowsa",
+    lastName : "Nur",
+    age: 17
+}
+
+let player2 = {
+    firstName: "Ikram",
+    lastName: "Nur",
+    age:10
+}
+ 
+console.log(player1.firstName)
+console.log(player2.firstName)
+
